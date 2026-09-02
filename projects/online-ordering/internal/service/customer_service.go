6bd8c09c4f855c5e5a64e61b1a19117efdb47d45
Vignesh Pugazhendhi <>
2026-09-01 22:48:40 -0400

    NewService added

--- projects/online-ordering/internal/service/customer_service.go
+++ projects/online-ordering/internal/service/customer_service.go
@@ -1,7 +1,32 @@
 package service
 
-import "github.com/vignesh/online-ordering/internal/repository"
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/vignesh/online-ordering/internal/models"
+	"github.com/vignesh/online-ordering/internal/repository"
+)
 
 type CustomerService struct {
 	repo repository.CustomerRepository
+}
+
+func (c *CustomerService) Create(ctx context.Context, customer *models.Customer) error {
+	customer.ID = uuid.New()
+	err := c.repo.Create(ctx, customer)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *CustomerService) GetByID(ctx context.Context, id uuid.UUID) error {
+	return c.repo.GetByID(ctx, id)
+}
+
+func NewCustomerService(customerRepository repository.CustomerRepository) *CustomerService {
+	return &CustomerService{
+		repo: customerRepository,
+	}
 }
